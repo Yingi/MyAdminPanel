bdfd664314336ee40e2c8fceb24bf7fdd9db579f
Yingi Kem <yingikeme@yahoo.com>
2018-11-13 02:14:58 -0800

    My own Admin Interface

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
-import { RestProvider, AuthProvider, base64Uploader } from '../lib';
-
-import { PostList, PostEdit, PostCreate } from './Posts';
-import { UserList, UserEdit, UserCreate } from './Users';
+import { RestProvider, base64Uploader } from '../lib';
+import { DriverList, DriverEdit, DriverCreate } from './Drivers';
+import { RidersList, RidersEdit, RidersCreate } from './Riders';
+import { TripList } from './Trip';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBIuQslUV4o-_6_z_NTty8HAFtSBJ0F9is',
-  authDomain: 'react-admin-firebase-client.firebaseapp.com',
-  databaseURL: 'https://react-admin-firebase-client.firebaseio.com',
-  projectId: 'react-admin-firebase-client',
-  storageBucket: '',
-  messagingSenderId: '627393424606'
+  apiKey: 'AIzaSyCXZiERpF2DscCApxbRca9Y7UtglIdc-G8',
+  authDomain: 'alpharides-f115c.firebaseapp.com',
+  databaseURL: 'https://alpharides-f115c.firebaseio.com/',
+  projectId: 'alpharides-f115c',
+  storageBucket: 'alpharides-f115c.appspot.com',
+  messagingSenderId: '38995523207'
 };
 
-const trackedResources = [{ name: 'posts', isPublic: true }, { name: 'users', isPublic: true }];
-
-const authConfig = {
-  userProfilePath: '/users/',
-  userAdminProp: 'isAdmin'
-};
+const trackedResources = [{ name: 'user', isPublic: true }, { name: 'drivers', isPublic: true }, { name: 'DriversWorking', isPublic: true }];
 
 // to run this demo locally, please feel free to disable authProvider to bypass login page
 
 const dataProvider = base64Uploader(RestProvider(firebaseConfig, { trackedResources }));
 const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={AuthProvider(authConfig)}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} />
+  <Admin dataProvider={dataProvider}>
+    <Resource name="user" list={RidersList} edit={RidersEdit} create={RidersCreate} />
+    <Resource name="drivers" list={DriverList} edit={DriverEdit} create={DriverCreate} />
+    <Resource name="DriversWorking" list={TripList} />
   </Admin>
 );
 export default App;
